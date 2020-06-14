b78bd7daf7332a52b25412a03b5923f71dced2a0
Domrev Igor <domrevigor@gmail.com>
2020-06-14 12:31:02 +0300

    cleaning logging output

--- cmd/common/configInitializer.go
+++ cmd/common/configInitializer.go
@@ -30,7 +30,7 @@ func init() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+		//fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 
 }
